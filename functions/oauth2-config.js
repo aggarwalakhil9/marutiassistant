c66f84105b6d8442893e60488ab91b51c3b69e6b
akhil aggarwal <aggarwal.akhil9@gmail.com>
2018-01-30 13:42:26 +0530

    initial commit

---
+++ functions/oauth2-config.js
@@ -0,0 +1,8 @@
+module.exports = {
+    // these are from the Google API console web interface
+    // https://code.google.com/apis/console
+    // (Section CLient ID for installed Application)
+    client_id: "83475248881-nfvfdrbaf5b0suq056e412oom5o13bgs.apps.googleusercontent.com",
+    client_secret: "uIGGoDCySMX3CQadXFpJeZ3a",    
+    redirect_uri: "https://oauth-redirect.googleusercontent.com/r/marutiassistant"
+};
